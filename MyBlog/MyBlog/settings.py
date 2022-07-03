4d0d004a21d53f1a94802d9b5ead91bc7a2ac409
aysedemirel <aysedemirel1996@gmail.com>
2022-07-03 16:04:30 +0300

    Refc

--- MyBlog/MyBlog/settings.py
+++ MyBlog/MyBlog/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'MyBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      # 'ENGINE': 'django.db.backends.sqlite3',
+      # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myBlog',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
     }
 }
 
